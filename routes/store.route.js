d2bd01d5f75135514315edc364df9909c2d2e776
kanomdook <kanung.2535@gmail.com>
2018-09-18 17:46:09 +0700

    [edit] store route

--- routes/store.route.js
+++ routes/store.route.js
@@ -2,6 +2,9 @@ const storeCtrl = require('../controllers/store.controller');
 const auth = require('../config/auth');
 
 module.exports = function (router) {
+    router.route('/store')
+        .post(auth.verify, storeCtrl.createStore);
+
     router.route('/store/:storeid')
         .get(auth.verify, storeCtrl.getDeatil);
 
